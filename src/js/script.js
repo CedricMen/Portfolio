44488f2d45cf154aec819c95fbc4c27a7b068706
CedricMen <cedricmenard1@gmail.com>
2021-09-16 14:11:48 -0400

    configuration des fonts/couleurs + debut html et nav

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    //hamburger
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+
+
 
 });
