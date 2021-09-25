b7a279973eab70028e65aaee26867fd60a073054
CedricMen <cedricmenard1@gmail.com>
2021-09-25 15:44:33 -0400

    curseur personnalise + suite page a-propos

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-
+    //menu
     var menu = document.querySelector('.menu');
     var closeMenu = document.querySelector('.closeMenu');
 
@@ -18,6 +18,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
         sideNav.style.width = '0%';
     }
 
+    //curseur
+    const cursor = document.querySelector('.cursor');
+
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style","top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+    document.addEventListener('click', () => {
+        cursor.classList.add("expand")
+
+        setTimeout(() => {
+            cursor.classList.remove("expand")
+        },500)
+    })
 
     //test
     console.log("Ça fonctionne!!!");
